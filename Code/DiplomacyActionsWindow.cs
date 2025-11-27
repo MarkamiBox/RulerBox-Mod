dee43323f48de36183d014aaf8b9331ef1eb1476
Salvatore Ignazio Novelli <Salvatore.novelli@studio.unibo.it>
2025-11-27 19:12:06 +0000

    Nov 27, 2025, 8:12 PM

--- Code/DiplomacyActionsWindow.cs
+++ Code/DiplomacyActionsWindow.cs
@@ -228,8 +228,8 @@ namespace RulerBox
             v.childForceExpandHeight = false;
 
             var le = col.AddComponent<LayoutElement>();
-            le.preferredWidth = 50f; 
-            le.minWidth = 50f; 
+            le.preferredWidth = 20f; 
+            le.minWidth = 20f; 
             le.flexibleWidth = 0f; 
             le.flexibleHeight = 1f;
 
